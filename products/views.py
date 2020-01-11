fed77f67135891fd84c3d108b86ca07344614cd3
Peters8090 <52279875+Peters8090@users.noreply.github.com>
2020-01-11 23:55:27 +0100

    Pagination improvements (not complete yet)

--- products/views.py
+++ products/views.py
@@ -12,14 +12,19 @@ from .forms import ProductSearch
 
 def products_list(request, username, page=1):
     products = Product.objects.filter(user__username__exact=username)
-    products = Paginator(products, Configuration.get_configuration().products_per_page).get_page(
-        page).object_list
+    products_paginator = Paginator(products, Configuration.get_configuration().products_per_page)
+    products_paginator_current_page = Paginator(products,
+                                                Configuration.get_configuration().products_per_page).get_page(page)
+
+    products = products_paginator.get_page(page).object_list
 
     form = ProductSearch()
 
     context = {
-        'products': products,
         'username': username,
+        'products': products,
+        'products_paginator': products_paginator,
+        'products_paginator_current_page': products_paginator_current_page,
         'form': form,
     }
     return render(request, 'products/products_list.html', context)
@@ -31,14 +36,19 @@ def products_list_search(request, username, page=1):
     else:
         products = Product.objects.filter(Q(name__contains=request.GET.get('query')) |
                                           Q(description__contains=request.GET.get('query')))
-        products = Paginator(products, Configuration.get_configuration().products_per_page).get_page(
-            page).object_list
+        products_paginator = Paginator(products, Configuration.get_configuration().products_per_page)
+        products_paginator_current_page = Paginator(products,
+                                                    Configuration.get_configuration().products_per_page).get_page(page)
+
+        products = products_paginator.get_page(page).object_list
 
         form = ProductSearch(request.GET or None)
 
         context = {
-            'products': products,
             'username': username,
+            'products': products,
+            'products_paginator': products_paginator,
+            'products_paginator_current_page': products_paginator_current_page,
             'form': form,
         }
         return render(request, 'products/products_list.html', context)
