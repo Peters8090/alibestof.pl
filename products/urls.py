6ec63895e49af62572485f99a3a0e0bec47c7b95
Peters8090 <52279875+Peters8090@users.noreply.github.com>
2020-01-12 21:41:17 +0100

    Merge two product_list views into one.

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ app_name = 'products'
 urlpatterns = [
     path('<str:username>/', views.products_list, name='products_list'),
     path('<str:username>/<int:page>', views.products_list, name='products_list'),
-    path('<str:username>/search/', views.products_list_search, name='products_list_search'),
-    path('<str:username>/search/<int:page>', views.products_list_search, name='products_list_search'),
+    path('<str:username>/search/', views.products_list, name='products_list_search'),
+    path('<str:username>/search/<int:page>', views.products_list, name='products_list_search'),
     path('<int:pk>', views.ProductDetailView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -10,15 +10,31 @@ from base.models import Configuration
 from .forms import ProductSearch
 
 
+# def products_list(request, username, page=1):
+#     form = ProductSearch()
+#
+#     context = {
+#         'username': username,
+#         'products': products,
+#         'products_paginator': products_paginator,
+#         'products_paginator_current_page': products_paginator_current_page,
+#         'form': form,
+#     }
+#     return render(request, 'products/products_list.html', context)
+
+
 def products_list(request, username, page=1):
     products = Product.objects.filter(user__username__exact=username)
+    if request.GET.get('query') is not None:
+        products = products.filter(Q(name__contains=request.GET.get('query')) |
+                                   Q(description__contains=request.GET.get('query')))
+
     products_paginator = Paginator(products, Configuration.get_configuration().products_per_page)
     products_paginator_current_page = Paginator(products,
                                                 Configuration.get_configuration().products_per_page).get_page(page)
-
     products = products_paginator.get_page(page).object_list
 
-    form = ProductSearch()
+    form = ProductSearch(request.GET or None)
 
     context = {
         'username': username,
@@ -26,32 +42,10 @@ def products_list(request, username, page=1):
         'products_paginator': products_paginator,
         'products_paginator_current_page': products_paginator_current_page,
         'form': form,
+        'request': request,
+        'abool': True,
     }
     return render(request, 'products/products_list.html', context)
-
-
-def products_list_search(request, username, page=1):
-    if request.GET.get('query') is None:
-        return HttpResponseRedirect(reverse('products:user_profile', args={username, }))
-    else:
-        products = Product.objects.filter(Q(name__contains=request.GET.get('query')) |
-                                          Q(description__contains=request.GET.get('query')))
-        products_paginator = Paginator(products, Configuration.get_configuration().products_per_page)
-        products_paginator_current_page = Paginator(products,
-                                                    Configuration.get_configuration().products_per_page).get_page(page)
-
-        products = products_paginator.get_page(page).object_list
-
-        form = ProductSearch(request.GET or None)
-
-        context = {
-            'username': username,
-            'products': products,
-            'products_paginator': products_paginator,
-            'products_paginator_current_page': products_paginator_current_page,
-            'form': form,
-        }
-        return render(request, 'products/products_list.html', context)
 
 
 class ProductDetailView(generic.DetailView):
