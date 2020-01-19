b7c79267f25fe882af413b6997f7124ad49c1205
Peters8090 <52279875+Peters8090@users.noreply.github.com>
2020-01-19 22:26:50 +0100

    Repair product details url

--- products/urls.py
+++ products/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 app_name = 'products'
 urlpatterns = [
+    path('<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    
     path('<str:username>/', views.products_list, name='products_list'),
     path('<str:username>/<int:page>/', views.products_list, name='products_list'),
     path('<str:username>/search/', views.products_list, name='products_list_search'),
     path('<str:username>/search/<int:page>/', views.products_list, name='products_list_search'),
     path('<str:username>/custom_page/', views.products_list_pagination_custom_page_redirect,
          name='products_list_pagination_custom_page_redirect'),
-    path('<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
