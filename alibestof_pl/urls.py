1febb2cbc79eea8959a81d9f2f80ec2d72229cef
Peters8090 <52279875+Peters8090@users.noreply.github.com>
2020-01-20 17:19:03 +0100

    Code refactor, pagination fixes

--- alibestof_pl/urls.py
+++ alibestof_pl/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import include, path
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('products/', include('products.urls')),
+    path('user/', include('products.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,7 +1,6 @@
 from django.db.models import Q
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, reverse, redirect
-from django.shortcuts import get_list_or_404
 from django.core.paginator import Paginator
 from django.views import generic
 from django.http import Http404
@@ -15,15 +14,16 @@ def products_list(request, username, page=1):
     products = Product.objects.filter(user__username__exact=username,
                                       published=True)
     if request.GET.get('query') is not None:
-        if request.GET.get('query') == "":
+        query = request.GET.get('query')
+        if query == "":
             return HttpResponseRedirect(
-                reverse('products:products_list', args={username, page}))
+                reverse('products:products_list', kwargs={'username': username, 'page': page, }))
 
         try:
-            products = products.filter(pk=int(request.GET.get('query')))
+            products = products.filter(pk=int(query))
         except ValueError:
-            products = products.filter(Q(name__icontains=request.GET.get('query')) |
-                                       Q(description__icontains=request.GET.get('query')))
+            products = products.filter(Q(name__icontains=query) |
+                                       Q(description__icontains=query))
 
     products_paginator = Paginator(products, Configuration.get_configuration().products_per_page)
     products_paginator_current_page = Paginator(products,
@@ -38,20 +38,20 @@ def products_list(request, username, page=1):
         'products_paginator': products_paginator,
         'products_paginator_current_page': products_paginator_current_page,
         'product_search_form': product_search_form,
-        'request_text': request.build_absolute_uri().split('/')[-1].translate(str.maketrans('', '', '0123456789')),
+        'request_text': request.build_absolute_uri().split('/')[-1].translate(
+            str.maketrans('', '', '0123456789')),
     }
     return render(request, 'products/products_list.html', context)
 
 
-# do not touch, leave it as is!
 def products_list_pagination_custom_page_redirect(request, username):
     try:
         page = int(request.GET.get('page'))
     except ValueError:
         page = 1
 
-    reverse_url = reverse('products:products_list', args=(username, page))
-    return redirect(reverse_url + request.GET.get('request_text'))
+    return redirect(reverse('products:products_list', kwargs={'username': username, 'page': page, }) + request.GET.get(
+        'request_text'))
 
 
 class ProductDetailView(generic.DetailView):
