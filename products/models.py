2aa7d0c374866e4cb2886ae8fd82846f0cb4cfda
Peters8090 <52279875+Peters8090@users.noreply.github.com>
2020-01-22 20:38:56 +0100

    Add cookies script. Revert previous commit. Add another way to view all products. Remove reflink from Product model. Description auto wrap. Description url auto replace. Minor fixes.

--- products/models.py
+++ products/models.py
@@ -5,16 +5,28 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=2000, blank=True)
-    image = models.ImageField(upload_to='')
-    reflink = models.URLField(max_length=1000, blank=True)
-    link = models.URLField(max_length=1000, blank=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='products/')
+    link = models.URLField(max_length=1000)
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
     date_modified = models.DateTimeField(auto_now=True, editable=False)
     published = models.BooleanField(default=True)
 
+    class Meta:
+        ordering = ['-date_created']
+
     def __str__(self):
         return self.name
 
-    class Meta:
-        ordering = ['-date_created']
+    def save(self, *args, **kwargs):
+        def replace_url(text, text2):
+            if text[:4] == text2:
+                text = "." + text
+            text = text.replace(f' {text2}', f'.{text2}')
+            text = text.replace(f'\n{text2}', f'\n.{text2}')
+
+            return text
+
+        self.description = replace_url(self.description, 'http')
+        self.description = replace_url(self.description, 'www')
+        super(Product, self).save(*args, *kwargs)
