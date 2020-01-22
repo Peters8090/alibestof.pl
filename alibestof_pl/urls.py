b980a4bfbee0bd394ef1f3d0bc6e55275efd4f0b
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2020-01-22 15:24:51 +0100

    Create product info page

--- alibestof_pl/urls.py
+++ alibestof_pl/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('user/', include('products.urls')),
+    path('products/', include('products.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
